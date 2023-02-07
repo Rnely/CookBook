2d98639bd8f77211971d60535510943201d7aaae
ricards <ricards.sinkjavics@accenture.com>
2023-02-07 16:02:12 +0200

    Made a basic navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/"/>
+            <Route path="/create"/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
