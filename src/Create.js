df00ad0c2d271092383a0beed6631ba28a8673c8
ricards <ricards.sinkjavics@accenture.com>
2023-02-07 16:49:52 +0200

    Defined states and made a form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,50 @@
+import { useState } from "react";
 
 
 const Create = () => {
+    const [ title, setTitle] = useState('')
+    const [ ingredients, setIngredients ] = useState([])
+    const [ method, setMethod ] = useState('')
+    const [ time, setTime ] = useState('')
 
+
+
+    return (
+        <div className="create">
+            <h2>Add a new recepie</h2>
+            <form >
+                <label>Recepie title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Recepie ingredients:</label>
+                <input 
+                    type="text"
+                    required
+                    value={ingredients}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <button>Add</button>
+                <label>Recepie method:</label>
+                <textarea 
+                    required
+                    value={method}
+                    onChange={(e) => setMethod(e.target.value)}
+                />
+                <label>Cooking time (in minutes):</label>
+                <input 
+                    type="number"
+                    required
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
+                />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default Create;
